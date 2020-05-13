87c165e4a499037851602be03a2a1ad90a801838
Thales Lessa <65303537+thalesglessa@users.noreply.github.com>
2020-05-13 15:53:36 -0300

    Update volume3cubes.py

--- Chapter 1/volume3cubes.py
+++ Chapter 1/volume3cubes.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-L = np.linspace(1, 3, 3)
-V = L**3
-
-plt.plot(L, V)
+L = np.linspace(1, 3, 3)   #Lenght of the cubes
+V = L**3                   #Volume of the cubes
+ 
+plt.plot(L, V)  
 plt.xlabel('Lenght (cm)')
 plt.ylabel('Volume (cm³)')
 plt.show
