b14cb53dc9ca9d76734f537abb95702d224ebaef
Thales Lessa <65303537+thalesglessa@users.noreply.github.com>
2020-05-13 15:45:30 -0300

    Create circumference_and_area.py

---
+++ Chapter 1/circumference_and_area.py
@@ -0,0 +1,7 @@
+from math import pi
+
+r = 2         #Radius of the circle
+C = 2*pi*r    #Circunference of the circle
+A = pi*r**2   #Area of the circle
+
+print(f'The circunference is {C}cm and the area is {A}cm²')
