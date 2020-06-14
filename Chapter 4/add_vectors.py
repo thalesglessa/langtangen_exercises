12be8a114ad19621e7401bd1f9d487626f7055e6
Thales Lessa <65303537+thalesglessa@users.noreply.github.com>
2020-06-14 14:19:59 -0300

    Update add_vectors.py

--- Chapter 4/add_vectors.py
+++ Chapter 4/add_vectors.py
@@ -1,7 +1,10 @@
 def sum_vectors(a, b):
-	c = []
-	c.append(a[0] + b[0])
-	return c[0]
+	if len(a) == len(b):
+		c = []
+		c.append(a[0] + b[0])
+		return c[0]
+	else:
+		return None
 
 a = [0]
 b = [0]
@@ -9,7 +12,4 @@ b = [0]
 a[0] = float(input('The first vector is: '))
 b[0] = float(input('The second vector is: '))
 
-if len(a) == len(b):
-	print(f'a is {a[0]} and b is {b[0]}, their sum is {sum_vectors(a, b)}')
-else:
-	print('None')
+print(f'a is {a[0]} and b is {b[0]}, their sum is {sum_vectors(a, b)}')
